ccd1166b41db72df9f46018c591f5c3850b7c79d
jisu <cscard1002@gmail.com>
2023-05-04 19:10:51 +0200

    fixed: error gnl

--- develope/srcs/parser/check_data.c
+++ develope/srcs/parser/check_data.c
@@ -7,10 +7,8 @@ char *get_path(char *line, t_cub *cub)
 
     p = line;
     p += 2;
-    printf("CURRENT P: %s", p);
     while (ft_isspace(*p))
         p++;
-    printf("CURRENT P: %s", p);
     fd = open(p, O_RDONLY);
     if (fd < 0)
         print_error("cannot open texture image file", cub);
@@ -38,23 +36,24 @@ int check_path(t_map *map, char *line, t_cub *cub)
     return (0);
 }
 
-int check_range_and_set(char *line, t_map *map, t_cub *cub)
+int check_range(char *line, t_map *map)
 {
     int i;
 
     i = 0;
     while (line[i])
-    {
-        i += skip_spacei(line + i);
+    {   
+        while (line[i] && (ft_isspace(line[i]) || line[i] == ','))
+            i++;
         if (ft_isdigit(line[i]))
         {
             if (!(ft_atoi(line + i) >= 0 && ft_atoi(line + i) <= 255))
-                print_error("rgb range error: (0-255)", cub);
+                return (1);
             while (ft_isdigit(line[i]))
                 i++;
         }
-        //put in the structure
     }
+    return (0);
 }
 
 int    check_syntax_rgb(char *line, t_map *map, t_cub *cub)
@@ -63,15 +62,14 @@ int    check_syntax_rgb(char *line, t_map *map, t_cub *cub)
     int comma;
     int count;
 
-    line += 2;
     i = 0;
     comma = 0;
     count = 0;
-    while (line[i])
+    while (line && line[i])
     {
         if (line[i] == ',')
             comma++;
-        if (!(ft_isspace(line[i]) || ft_isdigit(line[i]) || line[i] == ','))
+        if (!ft_isspace(line[i]) && !ft_isdigit(line[i]) && line[i] != ',')
             print_error("found non-numeric for rgb", cub);
         if (!ft_isdigit(line[i]) && ft_isdigit(line[i + 1]))
             count++;
@@ -79,20 +77,40 @@ int    check_syntax_rgb(char *line, t_map *map, t_cub *cub)
     }
     if (comma != 2 || count > 3)
         return (1);
-    return (check_range_and_set(line, map, cub));
+    return (check_range(line, map));
+}
+
+void    rgb_split(char *line, int rgb[3], char sep)
+{
+    int i;
+
+    i = 0;
+    while (line && *line && i < 3)
+    {
+        while (*line && (ft_isspace(*line) || *line == ','))
+            line++;
+        rgb[i] = ft_atoi(line);
+        i++;
+        while (*line && ft_isdigit(*line))
+            line++;
+    }
 }
 
 int check_color(t_map *map, char *line, char key, t_cub *cub)
 {
     int ret;
+    int i;
 
     ret = 0;
     line += 2;
     while (ft_isspace(*line))
         line++;
-    ret = check_syntax_rgb(line, map, cub);
-
-    // if (key == 'C')
-    // map->texture->rgb_c = rgb_split(line, ',');
+    if(check_syntax_rgb(line, map, cub))
+        print_error("rgb range error: (0-255)", cub);
+    i = 0;
+    if (key == 'C')
+        rgb_split(line, map->texture.rgb_c, ',');
+    else if (key == 'F')
+        rgb_split(line, map->texture.rgb_f, ',');
     return (ret);
 }
--- develope/srcs/parser/dup_map.c
+++ develope/srcs/parser/dup_map.c
@@ -14,13 +14,15 @@ int    check_wall(t_map *map, char **m, t_cub *cub)
             if (m[i][j] == '0'\
                 || m[i][j] == 'E' || m[i][j] == 'W' || m[i][j] == 'S' || m[i][j] == 'N')
             {
-                if (i == 0 || j == 0 || i == map->height || !m[i][j + 1])
+                if (i == 0 || j == 0 || i == map->height - 1 || !m[i][j + 1])
+                    return (1);
+                else if (m[i - 1][j] == ' ' || m[i + 1][j] == ' '\
+                    || m[i][j - 1] == ' ' || m[i][j + 1] == ' ')
                     return (1);
             }
-            else if (m[i - 1][j] == ' ' || m[i + 1][j] == ' '\
-                || m[i][j - 1] == ' ' || m[i][j + 1] == ' ')
-                return (1);
+            j++;
         }
+        i++;
     }
     return (0);
 }
@@ -28,21 +30,24 @@ int    check_wall(t_map *map, char **m, t_cub *cub)
 void    read_set_map(int fd, t_cub *cub, char **tmp)
 {
     int     i;
+    int     j;
     char    *line;
     char    *str;
 
     i = 0;
     line = NULL;
-    while (i < cub->map.height + 1)
+    while (i < cub->map.height)
     {
+        j = -1;
         line = get_next_line(fd);
-        if (!line)
-            print_error("gnl error: read_set_map", cub);
+        while (line && line[++j])
+            if (line[j] == '\n')
+                line[j] = '\0';
         str = (char *)malloc(sizeof(char) * (cub->map.width + 1));
         if (!str)
             print_error("malloc error: read_set_map", cub);
         ft_memset(str, ' ', cub->map.width);
-        str[cub->map.width + 1] = '\0';
+        str[cub->map.width] = '\0';
         ft_strcpy(str, line);
         tmp[i] = str;
         free(line);
@@ -53,7 +58,17 @@ void    read_set_map(int fd, t_cub *cub, char **tmp)
 
 void    read_til_start(int fd, t_cub *cub)
 {
-    // read til map start
+    char    *line;
+    int     i;
+
+    i = 0;
+    while (i < cub->map.start - 1)
+    {
+        line = get_next_line(fd);
+        free(line);
+        line = NULL;
+        i++;
+    }
 }
 
 void    dup_map(t_cub *cub, char *filename)
@@ -65,8 +80,8 @@ void    dup_map(t_cub *cub, char *filename)
         print_error("map not found", cub);
     if (cub->player.status == 0)
         print_error("player not found", cub);
-    cub->map.height = cub->map.mcount - cub->map.start;
-    tmp = (char **)malloc(sizeof(char *) * (cub->map.height + 2));
+    cub->map.height = cub->map.mcount - cub->map.start + 1;
+    tmp = (char **)malloc(sizeof(char *) * (cub->map.height + 1));
     if (!tmp)
         print_error("map temp malloc error", cub);
     fd = open(filename, O_RDONLY);
@@ -74,9 +89,13 @@ void    dup_map(t_cub *cub, char *filename)
         print_error("dup map file open error", cub);
     read_til_start(fd, cub);
     read_set_map(fd, cub, tmp);
-    tmp[cub->map.height + 1] = 0;
+    tmp[cub->map.height] = 0;
     cub->map.map = tmp;
+    for (int i = 0; i < 14; i++)
+        printf("map[%2d] : %s\n", i, cub->map.map[i]);
     if(check_wall(&cub->map, cub->map.map, cub))
         print_error("map error: opened wall", cub);
+    else
+        printf("map is ready\n");
     close(fd);
 }
