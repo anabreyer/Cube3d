6a52fb7a6a38e9f3fadcbee87a363d50b37f2c2c
Ana ADUAN BREYER <aaduan-b@e2r4p6.clusters.42paris.fr>
2023-05-23 14:29:15 +0200

    Captial letters

--- develope/srcs/init/init.c
+++ develope/srcs/init/init.c
@@ -1,145 +1,157 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaduan-b <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/23 14:22:22 by aaduan-b          #+#    #+#             */
+/*   Updated: 2023/05/23 14:22:29 by aaduan-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cub3d.h"
 
-void    init_key(t_key *key)
+void	init_key(t_key *key)
 {
-    key->w = 0;
-    key->s = 0;
-    key->a = 0;
-    key->d = 0;
-    key->turn_l = 0;
-    key->turn_r = 0;
+	key->w = 0;
+	key->s = 0;
+	key->a = 0;
+	key->d = 0;
+	key->turn_l = 0;
+	key->turn_r = 0;
 }
 
-void    init_sideDist(t_raycasting *ray, t_player *player)
+void	init_sideDist(t_raycasting *ray, t_player *player)
 {
-    if (ray->rayDir_x < 0)
-    {
-        ray->step_x = -1;
-        ray->sideDist_x = (player->pos_x - ray->map_x) * ray->deltaDist_x;
-    }
-    else
-    {
-        ray->step_x = 1;
-        ray->sideDist_x = (ray->map_x + 1.0 - player->pos_x) * ray->deltaDist_x;
-    }
-    if (ray->rayDir_y < 0)
-    {
-        ray->step_y = -1;
-        ray->sideDist_y = (player->pos_y - ray->map_y) * ray->deltaDist_y;
-    }
-    else
-    {
-        ray->step_y = 1;
-        ray->sideDist_y = (ray->map_y + 1.0 - player->pos_y) * ray->deltaDist_y;
-    }
+	if (ray->raydir_x < 0)
+	{
+		ray->step_x = -1;
+		ray->sidedist_x = (player->pos_x - ray->map_x) * ray->deltadist_x;
+	}
+	else
+	{
+		ray->step_x = 1;
+		ray->sidedist_x = (ray->map_x + 1.0 - player->pos_x) * ray->deltadist_x;
+	}
+	if (ray->raydir_y < 0)
+	{
+		ray->step_y = -1;
+		ray->sidedist_y = (player->pos_y - ray->map_y) * ray->deltadist_y;
+	}
+	else
+	{
+		ray->step_y = 1;
+		ray->sidedist_y = (ray->map_y + 1.0 - player->pos_y) * ray->deltadist_y;
+	}
 }
 
-void    init_ray(t_cub *cub, int x)
+void	init_ray(t_cub *cub, int x)
 {
-    double  camera;     // x-coordinate in camera space
+	double	camera;		// x-coordinate in camera space
 
-    camera = 2 * x / (double)WWIDTH - 1;    // raycasting tutorial page 3
-    if (x == (WWIDTH - 1))
-        camera = 1.0;
-    cub->ray.rayDir_x = cub->player.dir_x + cub->player.plane_x * camera;   // rayDir_x & rayDir_y: direction vector of ray
-    cub->ray.rayDir_y = cub->player.dir_y + cub->player.plane_y * camera;
-    cub->ray.map_x = (int)cub->player.pos_x;
-    cub->ray.map_y = (int)cub->player.pos_y;
-    // printf("\n======> PLAYER pos_X: %d, pox_Y: %d\n\n", (int)cub->player.pos_x, (int)cub->player.pos_y);
-                                                            // simplified fomula to get deltaDistX,Y : deltaDistX = abs(1 / rayDirX)
-    cub->ray.deltaDist_x = fabs(1 / cub->ray.rayDir_x);     // fabs: function to get an absolute value of real numbers  abs: for integar
-    cub->ray.deltaDist_y = fabs(1 / cub->ray.rayDir_y);
-    init_sideDist(&cub->ray, &cub->player);
+	camera = 2 * x / (double)WWIDTH - 1;	// raycasting tutorial page 3
+	if (x == (WWIDTH - 1))
+		camera = 1.0;
+	cub->ray.raydir_x = cub->player.dir_x + cub->player.plane_x * camera;	// raydir_x & raydir_y: direction vector of ray
+	cub->ray.raydir_y = cub->player.dir_y + cub->player.plane_y * camera;
+	cub->ray.map_x = (int)cub->player.pos_x;
+	cub->ray.map_y = (int)cub->player.pos_y;
+	// printf("\n======> PLAYER pos_X: %d, pox_Y: %d\n\n", (int)cub->player.pos_x, (int)cub->player.pos_y);
+															// simplified fomula to get deltaDistX,Y : deltaDistX = abs(1 / rayDirX)
+	cub->ray.deltadist_x = fabs(1 / cub->ray.raydir_x);		// fabs: function to get an absolute value of real numbers	abs: for integar
+	cub->ray.deltadist_y = fabs(1 / cub->ray.raydir_y);
+	init_sideDist(&cub->ray, &cub->player);
 }
 
 void init_map(t_map *map, t_cub *cub)
 {
-    int i;
+	int i;
 
-    map->map = NULL;
-    map->width = 0;
-    map->height = 0;
-    map->mcount = 0;
-    map->start = 0;
-    i = 0;
-    while (i < 4)
-        map->img_path[i++] = NULL;
-    i = 0;
-    while (i < 3)
-    {
-        map->rgb_c[i] = -1;
-        map->rgb_f[i] = -1;
-        i++;
-    }
-    map->floor = 0;
-    map->ceiling = 0;
-    map->minimap = 1;
+	map->map = NULL;
+	map->width = 0;
+	map->height = 0;
+	map->mcount = 0;
+	map->start = 0;
+	i = 0;
+	while (i < 4)
+		map->img_path[i++] = NULL;
+	i = 0;
+	while (i < 3)
+	{
+		map->rgb_c[i] = -1;
+		map->rgb_f[i] = -1;
+		i++;
+	}
+	map->floor = 0;
+	map->ceiling = 0;
+	map->minimap = 1;
 }
 
-void    init_img_sub(t_image *img, t_cub *cub)
+void	init_img_sub(t_image *img, t_cub *cub)
 {
-    int i;
-    int j;
+	int i;
+	int j;
 
-    i = 0;
-    while (i < WHEIGHT)
-    {
-        j = 0;
-        while (j < WWIDTH)
-            img->buffer[i][j++] = 0;
-        i++;
-    }
-    img->img_arr = (int **)malloc(4 * sizeof(int *));
-    if (!img->img_arr)
-        print_error("error: init_img_sub: img_arr malloc failed", cub);
-    i = 0;
-    while (i < 4)
-    {
-        img->img_arr[i] = (int *)malloc(sizeof(int) * 64 * 64);
-        if (!img->img_arr[i])
-            print_error("error: init_img_sub: img_arr malloc2 failed", cub);
-        ft_memset(img->img_arr[i], 0, (sizeof(int) * 64 * 64));
-        i++;
-    }
+	i = 0;
+	while (i < WHEIGHT)
+	{
+		j = 0;
+		while (j < WWIDTH)
+			img->buffer[i][j++] = 0;
+		i++;
+	}
+	img->img_arr = (int **)malloc(4 * sizeof(int *));
+	if (!img->img_arr)
+		print_error("error: init_img_sub: img_arr malloc failed", cub);
+	i = 0;
+	while (i < 4)
+	{
+		img->img_arr[i] = (int *)malloc(sizeof(int) * 64 * 64);
+		if (!img->img_arr[i])
+			print_error("error: init_img_sub: img_arr malloc2 failed", cub);
+		ft_memset(img->img_arr[i], 0, (sizeof(int) * 64 * 64));
+		i++;
+	}
 }
 
 void init_image(t_image *img, t_cub *cub)
 {
-    img->img = NULL;
-    img->data = NULL;
-    img->img_arr = NULL;
-    img->bpp = 0;
-    img->endian = 0;
-    img->size_line = 0;
-    init_img_sub(img, cub);
+	img->img = NULL;
+	img->data = NULL;
+	img->img_arr = NULL;
+	img->bpp = 0;
+	img->endian = 0;
+	img->size_line = 0;
+	init_img_sub(img, cub);
 }
 
 void init_player(t_player *player)
 {
-    player->dir_x = 0;
-    player->dir_y = 0;
-    player->plane_x = 0;
-    player->plane_y = 0;
-    if (player->status == NO)
-    {
-        player->dir_y = -1;
-        player->plane_x = 0.66;
-    }
-    else if (player->status == SO)
-    {
-        player->dir_y = 1;
-        player->plane_x = -0.66;
-    }
-    else if (player->status == WE)
-    {
-        player->dir_x = -1;
-        player->plane_y = -0.66;
-    }
-    else if (player->status == EA)
-    {
-        player->dir_x = 1;
-        player->plane_y = 0.66;
-    }
-    player->speed_move = 0.06;
-    player->speed_rotate = 0.05;
+	player->dir_x = 0;
+	player->dir_y = 0;
+	player->plane_x = 0;
+	player->plane_y = 0;
+	if (player->status == NO)
+	{
+		player->dir_y = -1;
+		player->plane_x = 0.66;
+	}
+	else if (player->status == SO)
+	{
+		player->dir_y = 1;
+		player->plane_x = -0.66;
+	}
+	else if (player->status == WE)
+	{
+		player->dir_x = -1;
+		player->plane_y = -0.66;
+	}
+	else if (player->status == EA)
+	{
+		player->dir_x = 1;
+		player->plane_y = 0.66;
+	}
+	player->speed_move = 0.06;
+	player->speed_rotate = 0.05;
 }
--- develope/srcs/raycasting/raycasting.c
+++ develope/srcs/raycasting/raycasting.c
@@ -8,15 +8,15 @@ void    calculate_dda(t_raycasting *ray, t_map *map, t_player *player)
     while (hit == 0)                                // until it hits the wall
     {
                                                     // jump to next map square, OR in x-direction, OR in y-direction
-        if (ray->sideDist_x < ray->sideDist_y)      // if the ray hit the x-side wall, side = 0 |
+        if (ray->sidedist_x < ray->sidedist_y)      // if the ray hit the x-side wall, side = 0 |
         {
-            ray->sideDist_x += ray->deltaDist_x;    // map_x : which wall among x-side walls
+            ray->sidedist_x += ray->deltadist_x;    // map_x : which wall among x-side walls
             ray->map_x += ray->step_x;
             ray->side = 0;
         }
         else                                        // else the ray hit the y-side wall, side = 1 __
         {
-            ray->sideDist_y += ray->deltaDist_y;
+            ray->sidedist_y += ray->deltadist_y;
             ray->map_y += ray->step_y;              // map_x : which wall among x-side walls
             ray->side = 1;
         }
@@ -26,18 +26,18 @@ void    calculate_dda(t_raycasting *ray, t_map *map, t_player *player)
     // raycasting tutorial page 3!!
     // we need to avoid fisheyed effect (Euclidean distance will give it)
     if (ray->side == 0)                             // if it hits x-side wall
-        ray->perpWallDist = ray->sideDist_x - ray->deltaDist_x;
-        // ray->perpWallDist = (ray->map_x - player->pos_x + (1 - ray->step_x) / 2) / ray->rayDir_x;
+        ray->perpwalldist = ray->sidedist_x - ray->deltadist_x;
+        // ray->perpwalldist = (ray->map_x - player->pos_x + (1 - ray->step_x) / 2) / ray->raydir_x;
     else
-        ray->perpWallDist = ray->sideDist_y - ray->deltaDist_y;
-        // ray->perpWallDist = (ray->map_y - player->pos_y + (1 - ray->step_y) / 2) / ray->rayDir_y;
-                                                    // deltaDist_x = |(1 / rayDir_x)|  
-                                            // (side = 0), perpWallDist = DistX / rayDir_x 
-                                                // perpWallDist = (mapX - posX + (1 - stepX) / 2) / rayDirX
-                                                // perpWallDist = xDist / rayDirX
+        ray->perpwalldist = ray->sidedist_y - ray->deltadist_y;
+        // ray->perpwalldist = (ray->map_y - player->pos_y + (1 - ray->step_y) / 2) / ray->raydir_y;
+                                                    // deltadist_x = |(1 / raydir_x)|  
+                                            // (side = 0), perpwalldist = DistX / raydir_x 
+                                                // perpwalldist = (mapX - posX + (1 - stepX) / 2) / rayDirX
+                                                // perpwalldist = xDist / rayDirX
                                             // (side = 1), 
-                                                // perpWallDist = (mapY - posY + (1 - stepY) / 2) / rayDirY
-                                                    // line_height = height / perpWallDist
+                                                // perpwalldist = (mapY - posY + (1 - stepY) / 2) / rayDirY
+                                                    // line_height = height / perpwalldist
 }
 
 /*
@@ -76,24 +76,24 @@ void    pre_buffer(t_raycasting *ray, t_player *player)
     double  wall;
 
     if (ray->side == 0)
-        wall = player->pos_y + (ray->perpWallDist * ray->rayDir_y);
+        wall = player->pos_y + (ray->perpwalldist * ray->raydir_y);
     else
-        wall = player->pos_x + (ray->perpWallDist * ray->rayDir_x);
+        wall = player->pos_x + (ray->perpwalldist * ray->raydir_x);
     wall = wall - floor(wall);
     ray->texture_x = (int)(wall * (double)64);
-    if (ray->side == 0 && ray->rayDir_x < 0)
+    if (ray->side == 0 && ray->raydir_x < 0)
         ray->texture_x = 64 - ray->texture_x - 1;
-    if (ray->side == 1 && ray->rayDir_y > 0)
+    if (ray->side == 1 && ray->raydir_y > 0)
         ray->texture_x = 64 - ray->texture_x - 1;
-    ray->line_height = (int)(WHEIGHT / ray->perpWallDist);
-    ray->startPoint = WHEIGHT / 2 - ray->line_height / 2;
-    ray->endPoint = WHEIGHT / 2 + ray->line_height / 2;
-    if (ray->startPoint < 0)
-        ray->startPoint = 0;
-    if (ray->endPoint >= WHEIGHT)
-        ray->endPoint = WHEIGHT;
+    ray->line_height = (int)(WHEIGHT / ray->perpwalldist);
+    ray->startpoint = WHEIGHT / 2 - ray->line_height / 2;
+    ray->endpoint = WHEIGHT / 2 + ray->line_height / 2;
+    if (ray->startpoint < 0)
+        ray->startpoint = 0;
+    if (ray->endpoint >= WHEIGHT)
+        ray->endpoint = WHEIGHT;
     ray->ratio = 1.0 * 64 / ray->line_height;
-    ray->texture_pos = (ray->startPoint - WHEIGHT / 2 + ray->line_height / 2) * ray->ratio;
+    ray->texture_pos = (ray->startpoint - WHEIGHT / 2 + ray->line_height / 2) * ray->ratio;
     
 }
 
@@ -102,21 +102,21 @@ void    set_buffer(t_raycasting *ray, t_image *img, int x)
     int y;
     int color;
 
-    y = ray->startPoint;
-    while (y < ray->endPoint)
+    y = ray->startpoint;
+    while (y < ray->endpoint)
     {
         ray->texture_y = (int)ray->texture_pos & (64 - 1);
         if (ray->side == 0)
         {
-            // printf("condition test: %d      tex_x:%d   tex_y: %d dir_x: %f dir_y: %f\n", 64 * ray->texture_y + ray->texture_x, ray->texture_x, ray->texture_y, ray->rayDir_x, ray->rayDir_y);
-            if (ray->rayDir_x >= 0)
+            // printf("condition test: %d      tex_x:%d   tex_y: %d dir_x: %f dir_y: %f\n", 64 * ray->texture_y + ray->texture_x, ray->texture_x, ray->texture_y, ray->raydir_x, ray->raydir_y);
+            if (ray->raydir_x >= 0)
                 color = img->img_arr[EA - 1][64 * ray->texture_y + ray->texture_x];
             else
                 color = img->img_arr[WE - 1][64 * ray->texture_y + ray->texture_x];
         }
         else if (ray->side == 1)
         {
-            if (ray->rayDir_y >= 0)
+            if (ray->raydir_y >= 0)
                 color = img->img_arr[SO - 1][64 * ray->texture_y + ray->texture_x];
             else
                 color = img->img_arr[NO - 1][64 * ray->texture_y + ray->texture_x];
@@ -145,21 +145,21 @@ void    paint_untextured_wall(t_raycasting *ray, t_cub *cub, int x)
 {
     int color;
 
-    ray->line_height = (int)WHEIGHT / ray->perpWallDist;
+    ray->line_height = (int)WHEIGHT / ray->perpwalldist;
 
-    ray->startPoint = WHEIGHT / 2 - ray->line_height / 2;
-    ray->endPoint = WHEIGHT / 2 + ray->line_height / 2;
-    if (ray->startPoint < 0)
-        ray->startPoint = 0;
-    if (ray->endPoint >= WHEIGHT)
-        ray->endPoint = WHEIGHT - 1;
+    ray->startpoint = WHEIGHT / 2 - ray->line_height / 2;
+    ray->endpoint = WHEIGHT / 2 + ray->line_height / 2;
+    if (ray->startpoint < 0)
+        ray->startpoint = 0;
+    if (ray->endpoint >= WHEIGHT)
+        ray->endpoint = WHEIGHT - 1;
 
     if (cub->map.map[ray->map_y][ray->map_x] == '1')
         color = 0x894a87;
     if (ray->side == 1)
         color = color / 2;
 
-    verLine(cub, x, ray->startPoint, ray->endPoint, color);
+    verLine(cub, x, ray->startpoint, ray->endpoint, color);
 }
 
 
